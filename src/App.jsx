834d53c41ac83c4ca51db11a972ff120e46952cd
Nahuel <nahuel.valentin@hotmail.com>
2023-08-31 19:42:13 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-import React, { useState,} from 'react';
-
+import React, { useState, useEffect } from 'react';
 
 const NFTGallery = () => {
-  const options = {
-    method: 'GET',
-    headers: {accept: 'application/json', 'X-API-KEY': '314a308105454f8db5ec7000a046c9e8'}
-  };
-  
-  fetch('https://api.opensea.io/v2/chain/Ethereum/contract/0x495f947276749Ce646f68AC8c248420045cb7b5e/nfts?limit=10', options)
+  const [nfts, setNFTs] = useState([]);
 
-  const [nfts] = useState([]);
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: { accept: 'application/json', 'X-API-KEY': '314a308105454f8db5ec7000a046c9e8' }
+    };
+
+    fetch('https://api.opensea.io/v2/chain/Ethereum/contract/0x495f947276749Ce646f68AC8c248420045cb7b5e/nfts?limit=10', options)
+      .then(response => response.json())
+      .then(data => {
+        setNFTs(data.assets);
+      })
+      .catch(error => {
+        console.error('Error al obtener los datos de los NFT:', error);
+      });
+  }, []);
 
   return (
     <div>
       <h1>Lista de NFTs</h1>
       <ul>
         {nfts.map(nft => (
-          <li key={nft.token_id}>
-            <img src={nft.image_url} alt={nft.name} />
+          <li key={nft.id}>
+            <img src={nft.image_preview_url} alt={nft.name} />
             <p>{nft.name}</p>
           </li>
         ))}
