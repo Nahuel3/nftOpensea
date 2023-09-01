3e2bad990b6bc5a5e5d8e00408b01c6e55b6a889
Nahuel <nahuel.valentin@hotmail.com>
2023-08-31 23:52:50 -0300

    dos colleciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './NFTGallery.css';
 
-
 const NFTGallery = () => {
-  const [nfts, setNFTs] = useState([]);
+  const [nftsFreedom, setNFTsFreedom] = useState([]);
+  const [nftsNAS, setNFTsNAS] = useState([]);
   const [offers, setOffers] = useState([]);
 
   useEffect(() => {
@@ -11,18 +11,28 @@ const NFTGallery = () => {
       method: 'GET',
       headers: { accept: 'application/json', 'X-API-KEY': '314a308105454f8db5ec7000a046c9e8' }
     };
+ 
+    // Coleccion FREEDOM
+    fetch('https://api.opensea.io/v2/collection/freedomk/nfts?limit=25', options)
+     .then(response => response.json())
+     .then(data => {
+       setNFTsFreedom(data.nfts);
+     })
+     .catch(error => {
+       console.error('Error al obtener los datos de los NFTs de FREEDOM:', error);
+     });
+     
+    // Coleccion NAS
+    fetch('https://api.opensea.io/v2/collection/nas/nfts?limit=5', options)
+     .then(response => response.json())
+     .then(data => {
+       setNFTsNAS(data.nfts);
+     })
+     .catch(error => {
+       console.error('Error al obtener los datos de los NFTs de NAS:', error);
+     });
 
-    fetch('https://api.opensea.io/v2/collection/freedomk/nfts?limit=50', options)
-      .then(response => response.json())
-      .then(data => {
-        setNFTs(data.nfts);
-        
-      })
-      .catch(error => {
-        console.error('Error al obtener los datos de los NFT:', error);
-      });
-
-      fetch('https://api.opensea.io/v2/offers/collection/freedomk', options)
+    fetch('https://api.opensea.io/v2/offers/collection/freedomk', options)
       .then(response => response.json())
       .then(data => {
         setOffers(data.offers);
@@ -36,7 +46,7 @@ const NFTGallery = () => {
     <div>
       <h1>Lista de NFTs</h1>
       <ul className="nft-gallery">
-        {nfts.map(nft => {
+        {[...nftsFreedom, ...nftsNAS].map(nft => {
           const offer = offers.find(offer => offer.id === nft.identifier);
           return (
             <li key={nft.identifier} className="nft-item">
